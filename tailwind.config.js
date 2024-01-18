0c730e45c8bddd391c62e69287bfa8a620af0626
Bern98 <90843464+Bern98@users.noreply.github.com>
2024-01-18 13:57:53 +0100

    Replaced video, lines are now centered and evenly spaced, fixed video in background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+		fontFamily: {
+			custom: "custom",
+			deadCRT: "deadCRT",
+			kongtext: "kongtext"
+		},
+		colors: {
+			'normal-gray': '#a8a8a8',
+			'highlight-gray': '#e6e6e6',
+		},
+		lineHeight: {
+			'extra-loose': '2.5'
+		}
+	},
   },
   plugins: [],
 }
