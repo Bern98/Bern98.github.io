339d3db244f7d067646cc23b351c7fa4e1cfeebf
Bern98 <90843464+Bern98@users.noreply.github.com>
2024-05-26 23:21:53 +0200

    Curriculum Page added and deployed initial italian layout + info

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
-  theme: {
-    extend: {
-		fontFamily: {
-			custom: "custom",
-			deadCRT: "deadCRT",
-			kongtext: "kongtext"
-		},
-		colors: {
-			'normal-gray': '#c0c0c0',
-			'highlight-gray': '#e6e6e6',
-		},
-		lineHeight: {
-			'extra-loose': '2.5'
-		}
+	content: ["./*.{html,js}", "./pages/*.html"],
+	theme: {
+	  extend: {
+		  fontFamily: {
+			  custom: "custom",
+			  deadCRT: "deadCRT",
+			  kongtext: "kongtext"
+		  },
+		  colors: {
+			  'normal-gray': '#c0c0c0',
+			  'highlight-gray': '#e6e6e6',
+		  },
+		  lineHeight: {
+			  'extra-loose': '2.5'
+		  }
+	  },
 	},
-  },
-  plugins: [],
-}
-
+	plugins: [],
+  }
+  
+  
